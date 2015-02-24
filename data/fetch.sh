964fdea6a41297938808380f51c4c4a46e0a4572
Jeffrey Meyers <meyers2@pdx.edu>
2015-02-24 10:10:38 -0800

    script to fetch smaller datas from trimet

---
+++ data/fetch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+stops=tm_stops
+park=tm_parkride
+tc=tm_tran_cen
+url=http://developer.trimet.org/gis/data
+
+function fetch() {
+    name=$1
+    wget ${url}/${name}.zip
+    unzip ${name}.zip
+    ogr2ogr -f "GeoJSON" ${name}.json ${name}.shp ${name}
+    mv ${name}.json ../
+    rm ./*
+}
+
+
+mkdir tmp
+cd tmp
+
+fetch ${stops}
+fetch ${park}
+fetch ${tc}
+
+cd ..
+rm -r tmp
+
+
