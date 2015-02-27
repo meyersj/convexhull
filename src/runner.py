b3ad47440d3dc19dc639cd8ea86c34c85a2c61b6
Jeffrey Meyers <meyers2@pdx.edu>
2015-02-26 19:26:08 -0800

    update

--- src/runner.py
+++ src/runner.py
@@ -9,18 +9,23 @@ BRUTE = "BruteForce"
 QUICK = "QuickHull"
 STD = "Standard"
 GIFT = "GiftWrap"
+ALGOS = [BRUTE, QUICK, GIFT, STD]
 
-GEN_SIZE = 50   # size of the data sets generated
+RAND = "random"
+TRI = "triangle"
+POLY = "polygon"
+SAMPLES = [RAND, TRI, POLY]
+
+# SET consist of multiple TESTS on the same data of the same size
 TRASH = 5        # this many of the first test times are ignored
-TESTS = 10       # num of times each dataset is tested
+TESTS = 20       # num of times each dataset is tested
 SETS = 10        # num of different datasets generated and tested for each test type
-
-# SETS consist of multiple TESTS of GEN_SIZE
 
 class Runner(object):
 
-    def __init__(self, outdir="output", outfile="results.csv"):
-        self.outfile = os.path.join(outdir, outfile)
+    def __init__(self, outdir="output"):
+        self.outdir = outdir
+        if not os.path.exists(self.outdir): os.makedirs(self.outdir)
         self.plotter = Plotter(outdir=outdir)
         self.brute = BruteForce()
         self.quick = QuickHull()
@@ -46,28 +51,20 @@ class Runner(object):
         print "VALIDATED!!"
 
    
-    def plot(self):
-        data = Gen.generate("random", GEN_SIZE)
-        quick = self.quick.algorithm(data)
-        brute = self.brute.algorithm(data)
-        gift = self.gift.algorithm(data)
-        std = self.std.algorithm(data)
-
-        self.plotter.save(QUICK, data, quick)
-        self.plotter.save(BRUTE, data, brute)
-        self.plotter.save(STD, data, std)
-        self.plotter.save(GIFT, data, gift)
-
+    def plot(self, sample):
+        data = Gen.generate(sample, 40)
+        hull = self.std.algorithm(data) 
+        self.plotter.save(sample, data, hull)
 
     def timeRuns(self, algo, data):
         avg = []
-        minVal = None
-        maxVal = 0
+        #minVal = None
+        #maxVal = 0
         for j in range(0, TESTS + TRASH):
             time = algo.time(data)
-            if not minVal: minVal = time
-            elif time <= minVal: minVal = time
-            if time >= maxVal: maxVal = time
+            #if not minVal: minVal = time
+            #elif time <= minVal: minVal = time
+            #if time >= maxVal: maxVal = time
             avg.append(time)
         avg = avg[TRASH:]
         return sum(avg) / float(len(avg))
@@ -80,24 +77,26 @@ class Runner(object):
         print "Tests:", TESTS
         print
 
-    def printResults(self, brute, quick, gift, std):
+    def printResults(self, brute, gift, quick, std):
         print "  ", BRUTE, brute, "ms"
-        print "  ", QUICK, quick, "ms"
         print "  ", GIFT, gift, "ms"
+        print "  ", QUICK, quick, "ms"
         print "  ", STD, std, "ms"
         print
 
     def writeResults(self, name, results):
-        with open(self.outfile, 'w') as f:
+        with open(os.path.join(self.outdir, name), 'w') as f:
             writer = csv.writer(f)
-            for algo in [BRUTE, QUICK, GIFT, STD]:
-                writer.writerow([algo, name] + results[algo])
+            writer.writerow(ALGOS)
+            for i in range(0, 10):
+                row = [ results[algo][i] for algo in ALGOS ]
+                writer.writerow(row)
 
-    def runSuite(self, sample):
+    def runSuite(self, sample, size):
         results = {BRUTE:[], QUICK:[], GIFT:[], STD:[]}
         for i in range(0, SETS):
             self.printHeader(i, sample)
-            data = Gen.generate(sample, GEN_SIZE)
+            data = Gen.generate(sample, size)
             # run tests
             brute = self.timeRuns(self.brute, data)
             quick = self.timeRuns(self.quick, data)
@@ -108,18 +107,33 @@ class Runner(object):
             results[QUICK].append(quick)
             results[GIFT].append(gift)
             results[STD].append(std)
-            self.printResults(brute, quick, gift, std)
-        self.writeResults(sample, results) 
+            self.printResults(brute, gift, quick, std)
+        self.writeResults(sample + str(size) + ".csv", results) 
 
-def main(outfile):
-    outdir = "../output"
-    test = "random"
-    runner = Runner(outdir=outdir, outfile=test+"200.csv")
+def getBaseDir():
+    return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    outdir = os.path.join(getBaseDir(), "output") 
+    runner = Runner(outdir=outdir)
     
-    #runner.runSuite(test) 
-    #runner.validate()
-    runner.plot()
+    suites = []
+    suites += [ (sample, 50) for sample in SAMPLES ]
+    #suites += [ (sample, 100) for sample in SAMPLES ]
+    #suites += [ (sample, 200) for sample in SAMPLES ]
+    #suites += [ (sample, 400) for sample in SAMPLES ]
+    #suites += [ (sample, 800) for sample in SAMPLES ]
+   
+    # run time tests
+    for sample, size in suites:
+        runner.runSuite(sample, size) 
+    
+    # generate visualization of different datasets
+    #for sample in SAMPLES:
+    #    runner.plot(sample)
 
+    #runner.validate()
 if __name__ == "__main__":
-    main("results.csv")
+    main()
 
